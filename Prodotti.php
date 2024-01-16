443eaecbcb36bcbc223fd894ec776bc753750928
Your Name <you@example.com>
2024-01-16 15:45:38 +0100

    .

--- Prodotti.php
+++ Prodotti.php
@@ -4,13 +4,17 @@ class Prodotti
     public $cibo;
     public $giochi;
     public $cuccie;
+    public $razza;
+    public $dimensioni;
 
-    public function __construct($cibo, $giochi, $ccuccie)
+    public function __construct($cibo, $giochi, $cuccie, Animali $razza, $dimensioni)
     {
 
         $this->cibo = $cibo;
         $this->giochi = $giochi;
-        $this->cuccie = $ccuccie;
+        $this->cuccie = $cuccie;
+        $this->razza = $razza;
+        $this->dimensioni = $dimensioni;
     }
 
     public function getFullAddress()
